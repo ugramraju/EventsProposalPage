3f781b371edb62617cc12e81e3c47691d84dc578
UGRAMRAJU <119936716+UGRAMRAJU@users.noreply.github.com>
2023-06-04 11:22:36 +0530

    body

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+const bodyParser = require("body-parser")
 const fileupload = require("express-fileupload");
 
 const userRouter = require("./Routes/userRoutes");
@@ -10,7 +11,12 @@ const proposalsRouter = require("./Routes/proposalsRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+app.use(
+  bodyParser.urlencoded({
+      extended: false
+  })
+);
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors({
   origin: "https://friendly-alpaca-87e68d.netlify.app",
